dab8c158025fb51a8f5ded66d58a2ad22bf39da0
Kotaru, Venkata (DS) <Venkata.Kotaru@cdk.com>
2016-03-11 17:41:39 +0530

    Updated data file

--- server.js
+++ server.js
@@ -31,18 +31,6 @@ app.get('/index.html', function(req, res) {
 app.use('/node_modules', express.static('node_modules'));
 app.use('/app', express.static('app'));
 
-// app.get('/node_modules/angular2/bundles/angular2-polyfills.js',function(req, res) {
-
-//     var reqPath = req.path;
-//     console.log(reqPath);
-//     var fileName = path.basename(reqPath);
-//     console.log(fileName);
-
-//     var newPath = "." + reqPath;
-//     var staticPage = fs.readFileSync(reqPath, 'utf8');
-//     res.send(200, staticPage);
-// });
-
 app.listen(3001, function(){
    console.log('node express server started'); 
 });
